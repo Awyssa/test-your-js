26d37ae8faa6e9b507e4e672fadf33ea5a414395
awyssa <50834877+Awyssa@users.noreply.github.com>
2024-09-24 22:35:43 +0100

    test 10

---
+++ test10.ts
@@ -0,0 +1,17 @@
+function logThis() {
+  console.log(this);
+}
+
+const obj = {
+  logThis,
+  logThis2() {
+    return logThis();
+  },
+  logThis3() {
+    return obj.logThis();
+  },
+};
+
+obj.logThis();
+obj.logThis2();
+obj.logThis3();
